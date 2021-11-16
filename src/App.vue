cdc158cb582d20f7d5a5909b0612bb32f873476b
taj1-takum1 <takumin390511@gmail.com>
2021-11-16 19:19:34 +0900

    props

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <LikeHeader></LikeHeader>
-    <LikeNumber></LikeNumber>
+    <h2>{{ number }}</h2>
+    <LikeNumber :number="number"></LikeNumber>
+    <LikeNumber :number="number"></LikeNumber>
   </div>
 </template>
 
@@ -9,8 +11,13 @@
 import LikeHeader from "./components/LikeHeader.vue";
 
 export default {
-components: {
-  LikeHeader
+  data() {
+    return {
+      number: 10
+    };
+  },
+  components: {
+    LikeHeader
   }
 };
 </script>
