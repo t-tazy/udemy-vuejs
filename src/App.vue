a1843b105c3fbf1c1dae8f045a25a8be1c70a230
taj1-takum1 <takumin390511@gmail.com>
2021-11-17 12:31:27 +0900

    "名前付きスロット"

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <LikeHeader>
+      <template v-slot:title>
+        <h2>こんにちは</h2>
+      </template>
+      <template v-slot:number>
+        <p>{{ number }}</p>
+      </template>
     </LikeHeader>
     <LikeNumber :number="number"></LikeNumber>
     <LikeNumber :number="number"></LikeNumber>
