eb68ef36f63c5fa0235cbe85b2151ca470dde4cc
taj1-takum1 <takumin390511@gmail.com>
2021-11-17 18:26:43 +0900

    ライフサイクルフック

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,15 @@
     <LikeNumber :number="number"></LikeNumber>
     <button @click="currentComponent = 'Home'">Home</button>
     <button @click="currentComponent = 'About'">About</button>
-    <component :is="currentComponent"></component>
+    <keep-alive>
+      <component :is="currentComponent"></component>
+    </keep-alive>
+    <div>
+      <h2>イベントのフォーム</h2>
+      <label for="title">タイトル</label>
+      <input id="title" type="text" v-model.lazy="eventData.title">
+      <p>{{ eventData.title }}</p>
+    </div>
   </div>
 </template>
 
@@ -20,7 +28,10 @@ export default {
   data() {
     return {
       number: 10,
-      currentComponent: "Home"
+      currentComponent: "Home",
+      eventData: {
+        title: "タイトル"
+      }
     };
   },
   components: {
@@ -30,9 +41,3 @@ export default {
   }
 };
 </script>
-
-<style scoped>
-div {
-  border: 1px solid blue;
-}
-</style>
