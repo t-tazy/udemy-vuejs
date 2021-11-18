c1cf3274926447649f1777804b5d51d7a89452ca
taj1-takum1 <takumin390511@gmail.com>
2021-11-18 15:39:15 +0900

    フィルター

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,30 @@
 <template>
-  <p v-border>Home</p>
+  <div>
+    <p v-border:solid.round.shadow="{width: '10px', color: 'blue'}">Home</p>
+    <h2>{{ title | upperCase }}</h2>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      tmpData: "hello",
+      title: "Welcome to Tokyo"
+    }
+  },
+  directives: {
+    border(el, binding) {
+      el.style.borderWidth = binding.value.width;
+      el.style.borderColor = binding.value.color;
+      el.style.borderStyle = binding.arg;
+      if (binding.modifiers.round) {
+        el.style.borderRadius = "0.5rem";
+      }
+      if (binding.modifiers.shadow) {
+        el.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.26)";
+      }
+    }
+  }
+};
+</script>
